f962dd80a6d5f3fd256aadeea303c2ab5bb10a66
KwonMbel <mbel@naver.com>
2023-04-05 18:20:11 +0900

    board interface 작성

--- src/main/java/com/music/sharemusic/service/BoardService.java
+++ src/main/java/com/music/sharemusic/service/BoardService.java
@@ -1,3 +1,26 @@
 package com.music.sharemusic.service;
 
-public interface BoardService {}
+import com.music.sharemusic.dto.BoardDto;
+import java.util.List;
+
+public interface BoardService {
+  public void putPost(BoardDto boardDto);
+
+  //게시글 등록
+
+  public BoardDto getPostOne(int postNo);
+
+  //view 에서 요청. 한 글 불러오기
+
+  public List<BoardDto> getPostAll();
+
+  //index 에서 요청. 모든 글 불러오기
+  //paging작업은 아직^^
+
+  public void updatePost(BoardDto boardDto);
+
+  //modify 에서 요청. 글 내용 수정하기
+
+  public void deletePost(BoardDto boardDto);
+  //view 에서 요청. 글 삭제하기
+}
--- src/main/java/com/music/sharemusic/service/BoardServiceImpl.java
+++ src/main/java/com/music/sharemusic/service/BoardServiceImpl.java
@@ -1,6 +1,35 @@
 package com.music.sharemusic.service;
 
+import com.music.sharemusic.dao.BoardDao;
+import com.music.sharemusic.dto.BoardDto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BoardServiceImpl implements BoardService {}
+public class BoardServiceImpl implements BoardService {
+
+  @Autowired
+  BoardDao boardDao;
+
+  @Override
+  public void putPost(BoardDto boardDto) {}
+
+  @Override
+  public BoardDto getPostOne(int postNo) {
+    BoardDto result = boardDao.getPostOne(postNo);
+    return result;
+  }
+
+  @Override
+  public List<BoardDto> getPostAll() {
+    List<BoardDto> result = boardDao.getPostAll();
+    return result;
+  }
+
+  @Override
+  public void updatePost(BoardDto boardDto) {}
+
+  @Override
+  public void deletePost(BoardDto boardDto) {}
+}
