2ba4276a883ef66cfce72a8eda71417b2013bc6f
KwonDev <mbel@naver.com>
2023-04-06 01:36:20 +0900

    Board Dto 작성

--- src/main/java/com/music/sharemusic/dto/BoardDto.java
+++ src/main/java/com/music/sharemusic/dto/BoardDto.java
@@ -1,3 +1,24 @@
 package com.music.sharemusic.dto;
 
-public class BoardDto {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDto {
+
+  private int postNo;
+  private String postAuth;
+  private String postTitle;
+  private String postCont;
+  private String postGenre;
+  private String postEmote;
+  private String postTags;
+  private int postLike;
+  private int postHits;
+  private String postDate;
+}
