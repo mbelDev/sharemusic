b16fd31a0659c3189ff5772dd36584b4ebf8366c
kim gilhwan <ghksl12q@gmail.com>
2023-04-10 16:52:15 +0900

    230410 프론트

--- src/main/resources/static/js/mainJs/mainPage.js
+++ src/main/resources/static/js/mainJs/mainPage.js
@@ -1,14 +1,29 @@
 
 // 스와이퍼
-  let swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 50,
-    loop: true,
-    autoplay: {   
-      delay: 2500, 
-      disableOnInteraction: false, 
-    },
-  });
+  // let swiper = new Swiper(".mySwiper", {
+  //   slidesPerView: "auto",
+  //   spaceBetween: 50,
+  //   loop: true,
+  //   autoplay: {   
+  //     delay: 2500, 
+  //     disableOnInteraction: false, 
+  //   },
+  // });
 
+  $('.swiper').each(function(index) {
+    t = $(this);
+    t.addClass('swiepr-' + index);
+  
+    let swiper = new Swiper( ".swiper", {
+      autoplay: {
+        delay: 1000, //add
+        disableOnInteraction: false,
+      },
+      speed: 2000,
+      loop: true,
+      loopAdditionalSlides: 1,
+      slidesPerView: "auto",
+    });
+  });
 
 
