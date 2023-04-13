7e00fa2ea768316cd313da2e9bdc319b0dc17555
kim gilhwan <ghksl12q@gmail.com>
2023-04-13 16:29:13 +0900

    likeButton recent

--- src/main/resources/static/js/board/view.js
+++ src/main/resources/static/js/board/view.js
@@ -1,9 +1,30 @@
-var swiper = new Swiper(".boardView-swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 4000, //add
-    disableOnInteraction: false,
-  },
-});
+
+window.onload = function(){
+  let swiper = new Swiper(".boardView-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 4000, //add
+      disableOnInteraction: false,
+    },
+  });
+
+  // let good = document.getElementsByClassName("good");
+  // let like = document.getElementsByClassName("like");
+  // werjwkrj();
+
+  // function werjwkrj(){
+  //   like.addEventListener("click", function(){
+  //     good.classList.toggle('liked')
+  //   });
+  // }
+  
+}
+
+function test(){
+  document.querySelector(".good-img").classList.toggle('liked');
+}
+
+
+
