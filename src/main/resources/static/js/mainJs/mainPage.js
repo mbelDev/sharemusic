a0a861a788ccb66eeb392efe76ac9dc08329de2a
kim gilhwan <ghksl12q@gmail.com>
2023-04-17 16:27:01 +0900

    오늘 끝

--- src/main/resources/static/js/mainJs/mainPage.js
+++ src/main/resources/static/js/mainJs/mainPage.js
@@ -1,32 +1,35 @@
 
 // 스와이퍼
-// var swiper = new Swiper(".profileMySwiper", {
-//   speed: 3000,
-//   loop: true,
-//   slidesPerView: 5,
-//   spaceBetween: 10,
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false,
-//   },
-// });
 
   $('.swiper').each(function(index) {
     t = $(this);
     t.addClass('swiepr-' + index);
   
     let swiper = new Swiper( ".swiper", {
+      speed: 3000,
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 10,
       autoplay: {
         delay: 3000, //add
         disableOnInteraction: false,
-      },
-      mousewheel: {
-        invert: false,
-      },
+      }
+    });
+  });
+
+  $('.profileMySwiper').each(function(index) {
+    t = $(this);
+    t.addClass('profileMySwiper-' + index);
+  
+    let swiper = new Swiper( ".profileMySwiper", {
       speed: 3000,
       loop: true,
       slidesPerView: 5,
-      spaceBetween: 10,
+      // spaceBetween: 5,
+      autoplay: {
+        delay: 3000, //add
+        disableOnInteraction: false,
+      }
     });
   });
 
